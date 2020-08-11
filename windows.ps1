21ed9aa571859ce7f295d1f847df3b7945e67a55
MarvinJWendt <git@marvinjwendt.com>
2020-08-11 05:24:11 +0200

    feat: add windows install script

--- windows.ps1
+++ windows.ps1
@@ -1,1 +1,26 @@
+$releases = "https://api.github.com/repos/dops-cli/dops/releases"
 
+Write-Host Determining latest release
+$tag = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].tag_name
+
+$download = "https://github.com/dops-cli/dops/releases/download/$tag/dops_Windows_x86_64.zip"
+$name = "dops_Windows_x86_64"
+$zip = "$env:TEMP\$name-$tag.zip"
+$dir = "$name-$tag"
+
+Write-Host Dowloading latest release
+Invoke-WebRequest $download -Out $zip
+
+Write-Host Extracting release files
+Expand-Archive $zip -DestinationPath "$Env:Programfiles\dops" -Force
+
+Write-Host Cleaning up target dir
+Remove-Item $name -Recurse -Force -ErrorAction SilentlyContinue
+
+Write-Host Updating path
+[Environment]::SetEnvironmentVariable(
+        "Path",
+        [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine) + ";$Env:Programfiles\dops",
+        [EnvironmentVariableTarget]::Machine)
+
+Write-Host Dops successfully installed!
